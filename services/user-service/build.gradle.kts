a79f9f30bb66bf020b2eebe1dd0da9e57b5a7723
Koisad <patrykpaczkow@gmail.com>
2026-02-13 20:31:29 +0100

    refactor(user): delete UserMessagingConfig and build implementations

--- services/user-service/build.gradle.kts
+++ services/user-service/build.gradle.kts
@@ -5,6 +5,4 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("io.projectreactor.netty:reactor-netty")
 }
